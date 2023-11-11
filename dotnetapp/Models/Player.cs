47d52f1a6245a224de4884193d8447e88c844faa
Iamneo <techadmin@iamneo.ai>
2023-11-11 09:41:17 +0000

    Backup Commit

--- dotnetapp/Models/Player.cs
+++ dotnetapp/Models/Player.cs
@@ -1,4 +1,22 @@
-﻿namespace dotnetapp.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace dotnetapp.Models
 {
-    
+    public class Player {
+        [Key]
+        public int Id{get;set;}
+        [Required(ErrorMessage="Name is required.")]
+        public string Name{get;set;}
+        public int Age{get;set;}
+        public string Category{get;set;}
+        [Required]
+        [Range(1,Int32.MaxValue,ErrorMessage="Bidding amount must be greater than 0.")]
+        public decimal BiddingAmount{get;set;}
+        [ForeignKey("Team")]
+        public int TeamId{get;set;}
+        public Team Team{get;set;}
+    }
 }
--- dotnetapp/Models/Team.cs
+++ dotnetapp/Models/Team.cs
@@ -1,5 +1,16 @@
-﻿namespace dotnetapp.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotnetapp.Models
 {
-    
+
+public class Team{
+    [Key]
+    public int Id{get;set;}
+    public string TeamName{get;set;}
+
+    public ICollection<Player>Players{get;set;}
+}
 
 }
