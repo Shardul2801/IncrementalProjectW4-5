1be352af31b26f085e87a352b7e9d46fcb612b21
Iamneo <techadmin@iamneo.ai>
2023-11-21 05:46:34 +0000

    Backup Commit

--- angularapp/src/app/edit-player/edit-player.component.ts
+++ angularapp/src/app/edit-player/edit-player.component.ts
@@ -8,11 +8,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./edit-player.component.css']
 })
 export class EditPlayerComponent implements OnInit {
-
+  id:number
+  playerdata:{id:number,name:string,age:number,category:string,biddingPrice:number}
   constructor(private adminservice:AdminService, private ar:ActivatedRoute) { }
-  const tid
+  
 
   ngOnInit(): void {
+    const tid = this.ar.snapshot.paramMap.get('id')
+    this.id = Number(tid)
+    this.getPlayer(this.id);
+  }
+
+  getPlayer(id:number){
+    this.adminservice.getPlayer(id).subscribe((data)=>{
+      this.playerdata = data;
+    })
+  }
+
+  onSave(player:Player){
+    this.playerdata = player
+    this.adminservice.editPlayer(this.playerdata).subscribe(()=>{
+      alert("Record Edited Successfully!")
+      console.log(this.playerdata);
+    })
   }
 
 }
